5eecd34fa8aab40d5fb321e8f75a7fb215b27859
EdwardMelendezM <192666@unsaac.edu.pe>
2023-09-01 20:31:59 -0500

    adding computed and effect

--- src/app/signal/pages/properties-page/properties-page.component.ts
+++ src/app/signal/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, computed, effect, signal} from '@angular/core';
+import {Data} from "../../interfaces/user.interface";
 
 @Component({
   selector: 'app-properties-page',
@@ -6,7 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./properties-page.component.css']
 })
 export class PropertiesPageComponent {
-  onFieldUpdated(field:String, value:string){
-    console.log({field,value})
+
+  public counter = signal(10)
+
+  public user = signal<Data>({
+    id:4564,
+    first_name:'asdsad',
+    avatar:'asdsa',
+    email:'asdsad',
+    last_name:'asdasd'
+
+  })
+
+  public fullName = computed(()=>{
+    return `${this.user().first_name} ${this.user().last_name}`
+  })
+
+  public userChangeEffect = effect(()=>{
+    console.log('User change effect trigger')
+    console.log(this.user().first_name)
+  })
+
+  onFieldUpdated(field:keyof Data, value:string){
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]:value
+    // })
+
+    this.user.update(current=>{
+      return {
+        ...current,
+        [field]:value
+      }
+    })
+    // this.user.mutate(current=>{
+    //   current.first_name= 'Hola mundo'
+    // })
   }
 }
